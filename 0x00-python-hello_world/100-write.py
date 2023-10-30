a464fe3abf6c6151e74f5decfd13f68ec84e90c1
DevBasem <engr.basemahmed@gmail.com>
2023-10-30 09:36:02 -0700

    task 11

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == '__main__':
+    error_message = "and that piece of art is \
+useful - Dora Korpar, 2015-10-19\n"
+
+sys.stderr.write(error_message)
+sys.exit(1)
