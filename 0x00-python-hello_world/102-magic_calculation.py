b70308ef1251bc79ca65f5bf95b43bf2f4bd15b0
DevBasem <engr.basemahmed@gmail.com>
2023-10-30 10:31:14 -0700

    task 13

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    base_value = 98
-    exponentiation_result = base_value ** a
-    final_result = exponentiation_result + b
-    return final_result
+    return (98 ** a + b)
