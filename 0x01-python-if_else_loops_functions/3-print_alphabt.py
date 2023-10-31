ceedbeba65d773669a6a7100ea6ed5e294264240
DevBasem <engr.basemahmed@gmail.com>
2023-10-31 16:37:10 -0700

    task 3

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for char_code in range(ord('a'), ord('z') + 1):
+    if char_code == ord('q') or char_code == ord('e'):
+        continue
+    else:
+        print("{}".format(chr(char_code)), end='')
