2469cea9e72ab8cc5f4c3af88b7ab18192ed3a47
DevBasem <engr.basemahmed@gmail.com>
2023-10-31 16:27:32 -0700

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alphabet in range(ord('a'), ord('z') + 1):
-    print(chr(alphabet), end="")
+for char_code in range(ord('a'), ord('z') + 1):
+    print("{}".format(chr(char_code)), end='')
