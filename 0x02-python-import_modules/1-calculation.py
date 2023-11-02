dedae04393ce6bba9e8f0fb5f18bd0452a053e3a
DevBasem <engr.basemahmed@gmail.com>
2023-11-02 15:45:45 -0700

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
- from calculator_1 import add, sub, mul, div
- a = 10
- b = 5
- result1 = add(a, b)
- result2 = sub(a, b)
- result3 = mul(a, b)
- result4 = div(a, b)
- print("{} + {} = {}".format(a, b, result1))
- print("{} - {} = {}".format(a, b, result2))
- print("{} * {} = {}".format(a, b, result3))
- print("{} / {} = {}".format(a, b, result4))
+
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    result1 = add(a, b)
+    result2 = sub(a, b)
+    result3 = mul(a, b)
+    result4 = div(a, b)
+    print ('{} + {} = {}'.format(a, b, add(a, b)))
+    print ('{} - {} = {}'.format(a, b, sub(a, b)))
+    print ('{} * {} = {}'.format(a, b, mul(a, b)))
+    print ('{} / {} = {}'.format(a, b, div(a, b)))
