653305e25d58f73cd89760269d17816443e08cd4
DevBasem <engr.basemahmed@gmail.com>
2023-11-02 15:23:39 -0700

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-a = 1
-b = 2
 
 if __name__ == "__main__":
     from add_0 import add
-    
+    a = 1
+    b = 2
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
-
