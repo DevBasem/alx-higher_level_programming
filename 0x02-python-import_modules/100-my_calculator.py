3e83b086dcaf0cc3e3b3909e77841739a0bac5a6
DevBasem <engr.basemahmed@gmail.com>
2023-11-02 18:43:43 -0700

    task 6 fixx

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
 
+import sys
+from calculator_1 import add, sub, mul, div
 
-    def is_valid_operator(operator):
-        return operator in ["+", "-", "*", "/"]
-
-
+def main():
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     a, operator, b = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    if not is_valid_operator(operator):
+    if operator not in ["+", "-", "*", "/"]:
         print("Unknown operator. Available operators: +, -, *, and /")
         sys.exit(1)
 
@@ -23,17 +19,22 @@ if __name__ == "__main__":
 
         if operator == "+":
             result = add(a, b)
+            print("{:d} + {:d} = {:d}".format(a, b, result))
         elif operator == "-":
             result = sub(a, b)
+            print("{:d} - {:d} = {:d}".format(a, b, result))
         elif operator == "*":
             result = mul(a, b)
+            print("{:d} * {:d} = {:d}".format(a, b, result))
         elif operator == "/":
             if b == 0:
                 print("Error: Division by zero is not allowed")
                 sys.exit(1)
             result = div(a, b)
-
-        print("{:d} {} {:d} = {:d}".format(a, operator, b, result))
+            print("{:d} / {:d} = {:d}".format(a, b, result))
     except ValueError:
         print("Error: Please enter valid integer inputs for 'a' and 'b")
         sys.exit(1)
+
+if __name__ == "__main__":
+    main()
