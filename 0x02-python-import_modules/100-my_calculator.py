ea0974b43576d25c668aafced885432bbcda744d
DevBasem <engr.basemahmed@gmail.com>
2023-11-02 17:35:42 -0700

    task 6 fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a, operator, b = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    if operator not in ('+', '-', '*', '/'):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    try:
+        a, b = int(a), int(b)
+    except ValueError:
+        print("Arguments should be integers.")
+        sys.exit(1)
+
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    else:
+        if b == 0:
+            print("Error: Division by zero")
+            sys.exit(1)
+        result = div(a, b)
+
+    print(f"{a} {operator} {b} = {result}")
 
 if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
-
-    operators = ["+", "-", "*", "/"]
-    argv = sys.argv[1:]
-    argc = len(argv)
-
-    if argc != 3:
-        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
-        sys.exit(1)
-
-    operator = argv[1]
-
-    if operator not in operators:
-        print("Unknown operator. Available operators: +, -, *, and /")
-        sys.exit(1)
-
-    a, b = int(argv[0]), int(argv[2])
-
-    if operator == "+":
-        result = add(a, b)
-        print("{:d} + {:d} = {:d}".format(a, b, result))
-    elif operator == "-":
-        result = sub(a, b)
-        print("{:d} - {:d} = {:d}".format(a, b, result))
-    elif operator == "*":
-        result = mul(a, b)
-        print("{:d} * {:d} = {:d}".format(a, b, result))
-    elif operator == "/":
-        result = div(a, b)
-        print("{:d} / {:d} = {:d}".format(a, b, result))
+    main()
 
