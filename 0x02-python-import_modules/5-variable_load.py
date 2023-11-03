d68470655c598ca87c2cbe3cb4a13fc420570056
DevBasem <engr.basemahmed@gmail.com>
2023-11-02 17:07:49 -0700

    task 5 fix

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-
-if __name__ != "__main__":
-    exit()
-
-print("{:d}".format(a))
-
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print("{}".format(a))
