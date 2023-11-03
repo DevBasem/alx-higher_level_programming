e4cd7315e4f3e6b181b96063147a2b2daf81a20d
DevBasem <engr.basemahmed@gmail.com>
2023-11-02 18:53:00 -0700

    task 6 fix 11

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     argv = sys.argv[1:]
     argv_count = len(argv)
     operators = ["+", "-", "*", "/"]
-    
     if argv_count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
@@ -17,14 +15,10 @@ if __name__ == "__main__":
         a = int(sys.argv[1])
         b = int(sys.argv[3])
         if sys.argv[2] == "+":
-            result = add(a, b)
-            print("{:d} + {:d} = {:d}".format(a, b, result))
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
         elif sys.argv[2] == "-":
-            result = sub(a, b)
-            print("{:d} - {:d} = {:d}".format(a, b, result))
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
         elif sys.argv[2] == "*":
-            result = mul(a, b)
-            print("{:d} * {:d} = {:d}".format(a, b, result))
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
         elif sys.argv[2] == "/":
-            result = div(a, b)
-            print("{:d} / {:d} = {:d}".format(a, b, result))
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
