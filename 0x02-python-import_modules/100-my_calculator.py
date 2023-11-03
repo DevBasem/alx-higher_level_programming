07b970ab0fae702ade3103b4a3ca500eefe81310
DevBasem <engr.basemahmed@gmail.com>
2023-11-02 18:22:00 -0700

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,36 +2,27 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+
+a = int(sys.argv[1])
+operator = sys.argv[2]
+b = int(sys.argv[3])
+
+if operator == '+':
+    result = add(a, b)
+elif operator == '-':
+    result = sub(a, b)
+elif operator == '*':
+    result = mul(a, b)
+elif operator == '/':
+    if b == 0:
+        print("Error: Division by zero")
         sys.exit(1)
+    result = div(a, b)
+else:
+    print("Unknown operator. Available operators: +, -, *, and /")
+    sys.exit(1)
 
-    a, operator, b = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    if operator not in ('+', '-', '*', '/'):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    try:
-        a, b = int(a), int(b)
-    except ValueError:
-        print("Arguments should be integers.")
-        sys.exit(1)
-
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == '*':
-        result = mul(a, b)
-    else:
-        if b == 0:
-            print("Error: Division by zero")
-            sys.exit(1)
-        result = div(a, b)
-
-    print("{:d} {} {:d} = {:d}".format(a, operator, b, result))
-
-if __name__ == "__main__":
-    main()
+print(f"{a} {operator} {b} = {result}")
