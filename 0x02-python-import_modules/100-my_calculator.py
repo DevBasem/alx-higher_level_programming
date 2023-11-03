eda5d0947ad0a7ad2405f80d5deed8f39a6f14d3
DevBasem <engr.basemahmed@gmail.com>
2023-11-02 18:40:36 -0700

    task 66

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
-    operators = ["+", "-", "*", "/"]
+
+    def is_valid_operator(operator):
+        return operator in ["+", "-", "*", "/"]
+
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -12,13 +14,12 @@ if __name__ == "__main__":
 
     a, operator, b = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    if operator not in operators:
+    if not is_valid_operator(operator):
         print("Unknown operator. Available operators: +, -, *, and /")
         sys.exit(1)
 
     try:
         a, b = int(a), int(b)
-        result = None
 
         if operator == "+":
             result = add(a, b)
@@ -27,10 +28,12 @@ if __name__ == "__main__":
         elif operator == "*":
             result = mul(a, b)
         elif operator == "/":
+            if b == 0:
+                print("Error: Division by zero is not allowed")
+                sys.exit(1)
             result = div(a, b)
 
-        if result is not None:
-            print("{:d} {} {:d} = {:d}".format(a, operator, b, result))
+        print("{:d} {} {:d} = {:d}".format(a, operator, b, result))
     except ValueError:
         print("Error: Please enter valid integer inputs for 'a' and 'b")
         sys.exit(1)
