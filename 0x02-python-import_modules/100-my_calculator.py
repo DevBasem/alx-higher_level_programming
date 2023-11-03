824c4d1c16d7aa5cc1d2bc11c1688a24dd240f75
DevBasem <engr.basemahmed@gmail.com>
2023-11-02 17:38:56 -0700

    task 6 fix 2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
 
 def main():
+    import sys
+    from calculator_1 import add, sub, mul, div
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
