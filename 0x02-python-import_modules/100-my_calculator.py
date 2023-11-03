b83940b3b42c46b00cf65d4868001664a7a7976b
DevBasem <engr.basemahmed@gmail.com>
2023-11-02 18:37:23 -0700

    task 6 fix 7

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
 
-import sys
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
-    operators = {"+": add, "-": sub, "*": mul, "/": div}
-    
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    operators = ["+", "-", "*", "/"]
+
     if len(sys.argv) != 4:
-        print("Usage: ./calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
-    a, operator, b = map(str, sys.argv[1:])
-    
+
+    a, operator, b = sys.argv[1], sys.argv[2], sys.argv[3]
+
     if operator not in operators:
         print("Unknown operator. Available operators: +, -, *, and /")
         sys.exit(1)
-    
+
     try:
-        a, b = float(a), float(b)
-        result = operators[operator](a, b)
-        print("{:.1f} {} {:.1f} = {:.1f}".format(a, operator, b, result))
+        a, b = int(a), int(b)
+        result = None
+
+        if operator == "+":
+            result = add(a, b)
+        elif operator == "-":
+            result = sub(a, b)
+        elif operator == "*":
+            result = mul(a, b)
+        elif operator == "/":
+            result = div(a, b)
+
+        if result is not None:
+            print("{:d} {} {:d} = {:d}".format(a, operator, b, result))
     except ValueError:
-        print("Error: Please enter valid numeric inputs for 'a' and 'b'")
+        print("Error: Please enter valid integer inputs for 'a' and 'b")
         sys.exit(1)
