0f8daa54817909ccfaf61edaf2d43624bb369832
DevBasem <engr.basemahmed@gmail.com>
2023-11-08 16:13:44 -0800

    advanced 1 2 3

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0.0
+
+    total_score = 0
+    total_weight = 0
+
+    for score, weight in my_list:
+        total_score += score * weight
+        total_weight += weight
+
+    if total_weight == 0:
+        return 0.0
+
+    return total_score / total_weight
