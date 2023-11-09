efc8558686c6b3ae267241ba085cb213dce30e21
DevBasem <engr.basemahmed@gmail.com>
2023-11-08 16:34:51 -0800

    task 4 adv 3

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,8 @@
-#include <Python.h>
 #include <stdio.h>
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
 
 /**
  * print_python_list - Print Python list info
