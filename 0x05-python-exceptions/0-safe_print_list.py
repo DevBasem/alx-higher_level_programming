affa55ef871b905e5b85a6b7e1a4324b675a96b4
DevBasem <engr.basemahmed@gmail.com>
2023-11-20 13:24:36 -0800

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    printed_elements = 0
+
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            printed_elements += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+
+    return printed_elements
