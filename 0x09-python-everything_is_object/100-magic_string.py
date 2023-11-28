3027ec0197d38b5243520747c09635bf23ec1a46
DevBasem <engr.basemahmed@gmail.com>
2023-11-28 15:20:08 -0800

    task 1 adv

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""Module magic_string
-Returns a string "Holberton" repeated n times.
-"""
-
-
 def magic_string():
-    """Returns a string "Holberton" repeated n times."""
     magic_string.c = getattr(magic_string, "c", 0) + 1
     return (("Holberton, " * magic_string.c)[:-2])
