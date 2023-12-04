65fd3d2e4189ba3d8d8ad1c12321607c7631c718
DevBasem <engr.basemahmed@gmail.com>
2023-12-04 15:34:28 -0800

    task 11 fx

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,28 +1,49 @@
 #!/usr/bin/python3
 """
-This module defines the Square class.
+Square Class Module
 
-Classes:
-    Square: A class that inherits from Rectangle.
+This module defines the Square class, a subclass of Rectangle.
 """
 
-Rectangle = __import__('9-rectangle').Rectangle
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
     """
-    A class representing a square, inheriting from Rectangle.
+    Square class
+
+    Attributes:
+        __size (int): Private attribute representing
+        the size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a Square
+        instance with a given size.
+        area(self): Computes the area of the square.
+        __str__(self): Returns a string representation of the square.
     """
 
     def __init__(self, size):
         """
-        Initializes a Square instance.
+        Initializes a square with a given size.
 
         Args:
             size (int): The size of the square.
         """
+        # Updated variable naming for clarity
         self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
+
+    def area(self):
+        """
+        Computes the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        # Updated variable naming for clarity
+        return self.__size ** 2
 
     def __str__(self):
         """
@@ -31,5 +52,5 @@ class Square(Rectangle):
         Returns:
             str: A string representation of the square.
         """
-        return "[Square] {}/{}".format
-        (self._Rectangle__width, self._Rectangle__height)
+        # Updated variable naming for clarity
+        return "[Square] {}/{}".format(self.__size, self.__size)
