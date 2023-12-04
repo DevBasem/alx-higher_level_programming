9167f6d4fabe47b996eaeb820f3f414f6db55439
DevBasem <engr.basemahmed@gmail.com>
2023-12-04 14:58:13 -0800

    task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines the BaseGeometry class.
+
+Classes:
+    BaseGeometry: An empty class.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class.
+    """
+    pass
