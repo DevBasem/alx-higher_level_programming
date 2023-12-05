d2383ef8b85fc14bb45abca9de5eafd534315194
DevBasem <engr.basemahmed@gmail.com>
2023-12-05 13:56:13 -0800

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF-8) and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        None
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
