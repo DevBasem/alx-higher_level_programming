e3118404648b6b367384ab1c7ec402a12329aa68
DevBasem <engr.basemahmed@gmail.com>
2023-12-05 15:07:02 -0800

    task 2 adv fx4

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,11 +6,13 @@ This script reads input from stdin line by line and computes metrics.
 
 Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
                                         <status code> <file size>
-Each 10 lines and after a keyboard interruption (CTRL + C), it prints the following statistics since the beginning:
+Each 10 lines and after a keyboard interruption (CTRL + C),
+it prints the following statistics since the beginning:
 - Total file size: File size: <total size>
 - Number of lines by status code:
     - Possible status codes: 200, 301, 400, 401, 403, 404, 405, and 500
-    - If a status code doesn’t appear, it doesn’t print anything for that status code
+    - If a status code doesn’t appear, it doesn’t print anything
+    for that status code
     - Format: <status code>: <number>
     - Status codes are printed in ascending order
 """
