f9465cd57ad55e04d4b2af1e6f1ed46acebd46bf
DevBasem <engr.basemahmed@gmail.com>
2023-12-05 14:33:52 -0800

    task 7 fx 2

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-"""7-add_item module"""
+"""add_item module"""
 
 import sys
-import json
-import os.path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
 
-filename = "add_item.json"
+def add_items_to_json_list(file_path):
+    """
+    Adds command-line arguments to a Python list and saves them to a JSON file.
 
-# Load existing list from the file or create an empty list
-my_list = load_from_json_file(filename)
+    Args:
+        file_path (str): The path to the JSON file.
 
-# Add command-line arguments to the list
-my_list.extend(sys.argv[1:])
+    Returns:
+        None
+    """
+    json_list = []
 
-# Save the updated list to the file
-save_to_json_file(my_list, filename)
+    if os.path.exists(file_path):
+        json_list = load_from_json_file(file_path)
+
+    for arg in sys.argv[1:]:
+        json_list.append(arg)
+
+    save_to_json_file(json_list, file_path)
