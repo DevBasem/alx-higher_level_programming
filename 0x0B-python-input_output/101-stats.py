4f0f7cfb5c6423f0d6ba597d841734785cc03104
DevBasem <engr.basemahmed@gmail.com>
2023-12-05 15:05:28 -0800

    task 2 adv fx3

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 """
-Module for log parsing scripts.
-"""
+101-stats module
 
+This script reads input from stdin line by line and computes metrics.
+
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+                                        <status code> <file size>
+Each 10 lines and after a keyboard interruption (CTRL + C), it prints the following statistics since the beginning:
+- Total file size: File size: <total size>
+- Number of lines by status code:
+    - Possible status codes: 200, 301, 400, 401, 403, 404, 405, and 500
+    - If a status code doesn’t appear, it doesn’t print anything for that status code
+    - Format: <status code>: <number>
+    - Status codes are printed in ascending order
+"""
 
 import sys
 
-
 if __name__ == "__main__":
-    size = [0]
+    size = 0
     codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
     def check_match(line):
@@ -16,7 +26,7 @@ if __name__ == "__main__":
         try:
             line = line[:-1]
             words = line.split(" ")
-            size[0] += int(words[-1])
+            size += int(words[-1])
             code = int(words[-2])
             if code in codes:
                 codes[code] += 1
@@ -25,10 +35,11 @@ if __name__ == "__main__":
 
     def print_stats():
         '''Prints accumulated statistics.'''
-        print("File size: {}".format(size[0]))
+        print("File size: {}".format(size))
         for k in sorted(codes.keys()):
             if codes[k]:
                 print("{}: {}".format(k, codes[k]))
+
     i = 1
     try:
         for line in sys.stdin:
