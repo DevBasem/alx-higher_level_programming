94edb44508547c1b23a24620f89d0e8ed46ac277
DevBasem <engr.basemahmed@gmail.com>
2023-12-05 15:19:32 -0800

    task 7 fx

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
 """add_item module"""
 
-import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from sys import argv
+from importlib import import_module
 
+load_file = import_module('6-load_from_json_file').load_from_json_file
+save_file = import_module('5-save_to_json_file').save_to_json_file
 
-def add_items_to_json_list(file_path):
-    """
-    Adds command-line arguments to a Python list and saves them to a JSON file.
-
-    Args:
-        file_path (str): The path to the JSON file.
-
-    Returns:
-        None
-    """
+try:
+    json_list = load_file('add_item.json')
+except (ValueError, FileNotFoundError):
     json_list = []
 
-    if os.path.exists(file_path):
-        json_list = load_from_json_file(file_path)
+for item in argv[1:]:
+    json_list.append(item)
 
-    for arg in sys.argv[1:]:
-        json_list.append(arg)
-
-    save_to_json_file(json_list, file_path)
+save_file(json_list, 'add_item.json')
