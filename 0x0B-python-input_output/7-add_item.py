bb1bf2fb5f6bc1832d916c35368e11e4b99ee5e5
DevBasem <engr.basemahmed@gmail.com>
2023-12-05 14:28:04 -0800

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""add_item module"""
+"""7-add_item module"""
 
 import sys
-from os import path
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
-# Check if the file exists
-if not path.exists(filename):
-    save_to_json_file([], filename)
-
-# Load the existing list from the file
+# Load existing list from the file or create an empty list
 my_list = load_from_json_file(filename)
 
-# Add arguments to the list
+# Add command-line arguments to the list
 my_list.extend(sys.argv[1:])
 
 # Save the updated list to the file
