e80eef208ccda2ba880c5cac932be4a5ff69148b
DevBasem <engr.basemahmed@gmail.com>
2023-12-05 15:03:14 -0800

    task 2 adv fx2

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,51 +1,41 @@
 #!/usr/bin/python3
 """
-101-stats module
-
-Reads input from stdin line by line and computes metrics.
-
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-                                        <status code> <file size>
-Every 10 lines and after a keyboard interruption (CTRL + C), it prints:
-- Total file size: File size: <total size>
-- Number of lines by status code:
-  - Possible status codes: 200, 301, 400, 401, 403, 404, 405, 500
-  - Format: <status code>: <number>, printed in ascending order
+Module for log parsing scripts.
 """
+
+
 import sys
 
+
 if __name__ == "__main__":
-    total_size = 0
-    status_counts =
-    {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    size = [0]
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
     def check_match(line):
         '''Checks for regexp match in line.'''
         try:
             line = line[:-1]
             words = line.split(" ")
-            size = int(words[-1])
+            size[0] += int(words[-1])
             code = int(words[-2])
-            if code in status_counts:
-                status_counts[code] += 1
-            return size
+            if code in codes:
+                codes[code] += 1
         except ValueError:
-            return 0
+            pass
 
     def print_stats():
         '''Prints accumulated statistics.'''
-        print("File size: {}".format(total_size))
-        for code in sorted(status_counts.keys()):
-            if status_counts[code]:
-                print("{}: {}".format(code, status_counts[code]))
-
-    line_count = 1
+        print("File size: {}".format(size[0]))
+        for k in sorted(codes.keys()):
+            if codes[k]:
+                print("{}: {}".format(k, codes[k]))
+    i = 1
     try:
         for line in sys.stdin:
-            total_size += check_match(line)
-            if line_count % 10 == 0:
+            check_match(line)
+            if i % 10 == 0:
                 print_stats()
-            line_count += 1
+            i += 1
     except KeyboardInterrupt:
         print_stats()
         raise
