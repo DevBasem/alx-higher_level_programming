41dddcccd96e5cfc41ce402b37084a7b33e4c5ff
DevBasem <engr.basemahmed@gmail.com>
2024-01-16 09:51:29 -0800

    task 1 adv

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- Convert hbtn_0c_0 database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+USE hbtn_0c_0
 -- Convert first_table table and the name field to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
