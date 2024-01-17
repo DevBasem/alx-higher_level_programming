192ed430f638c98c52d8d58111d58066a410394c
DevBasem <engr.basemahmed@gmail.com>
2024-01-17 15:22:30 -0800

    task 9 fx 2

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,12 +2,8 @@
 USE `hbtn_0d_usa`;
 
 -- Select cities with corresponding state names using a subquery
-SELECT
-    `city`.`id` AS `city_id`,
-    `city`.`name` AS `city_name`,
-    `state`.`name` AS `state_name`
-FROM
-    `cities` AS `city`
-    INNER JOIN `states` AS `state` ON `city`.`state_id` = `state`.`id`
-ORDER BY
-    `city`.`id`;
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
