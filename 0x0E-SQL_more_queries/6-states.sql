0f8e01c6428af86e6cc34b64f0656de8191183d7
DevBasem <engr.basemahmed@gmail.com>
2024-01-17 15:14:20 -0800

    task 6 fx

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,9 @@
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
+-- Use the database
+USE `hbtn_0d_usa`;
+
 -- Create the table states if it does not exist
 CREATE TABLE IF NOT EXISTS `states` (
   `id` INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
