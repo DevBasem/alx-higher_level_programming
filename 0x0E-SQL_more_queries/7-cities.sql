51722bed1b0920574410f2d758f6167ae09e3393
DevBasem <engr.basemahmed@gmail.com>
2024-01-17 15:16:19 -0800

    tasks 7 / 9 fx

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,3 +1,9 @@
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
+-- Use the database
+USE `hbtn_0d_usa`;
+
 -- Create the table cities if it does not exist
 CREATE TABLE IF NOT EXISTS `cities` (
   `id` INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,6 @@
+-- Use the database
+USE `hbtn_0d_usa`;
+
 -- Select cities with corresponding state names using JOIN
 SELECT `cities`.`id`, `cities`.`name`, `states`.`name` AS `state_name`
 FROM `cities`
