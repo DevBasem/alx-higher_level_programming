674bf442ec0df1810678c0913edbb657a4d69a9d
DevBasem <engr.basemahmed@gmail.com>
2024-01-17 14:57:05 -0800

    task 3 fx

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,9 +1,12 @@
--- 3-force_name.sql
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 
-USE hbtn_0d_2;
+-- Use the database
+USE `hbtn_0d_2`;
 
--- Create table force_name if it does not exist
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL
+-- Creates the table force_name if it does not exist.
+CREATE TABLE IF NOT EXISTS `force_name` (
+    `id` INT,
+    `name` VARCHAR(256) NOT NULL,
+    PRIMARY KEY (`id`)
 );
