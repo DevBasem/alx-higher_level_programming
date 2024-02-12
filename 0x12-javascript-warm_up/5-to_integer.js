15470fdea88d848b20c5d5e7fe1c0618173d642f
DevBasem <engr.basemahmed@gmail.com>
2024-02-12 15:01:25 -0800

    task 6

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@
 const numberOfArgs = process.argv.length - 1;
 
 if (isNaN(parseInt(process.argv[numberOfArgs]))) {
-  console.log ('Not a number');
+  console.log('Not a number');
 } else {
   console.log(`My number: ${parseInt(process.argv[numberOfArgs])}`);
 }
