0e40e33003028c5f01ee74c24acfa4836f85b5b9
DevBasem <engr.basemahmed@gmail.com>
2024-02-12 15:34:56 -0800

    task 9

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Print a square
+const character = 'x';
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log(character.repeat(size));
+  }
+}
