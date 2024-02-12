13ba9de8c8575ec545be208fd6a6f4883b5a9bcb
DevBasem <engr.basemahmed@gmail.com>
2024-02-12 15:09:46 -0800

    task 7

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Print some lines
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < strings.length; i++) {
+  console.log(strings[i]);
+}
