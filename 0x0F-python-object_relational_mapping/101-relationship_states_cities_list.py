18ba61773e7033d7d6a140857ff5fdfea674477a
DevBasem <engr.basemahmed@gmail.com>
2024-02-18 15:42:12 -0800

    task 2 adv fx 1

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,41 +1,24 @@
 #!/usr/bin/python3
-"""List all states with corresponding cities"""
-from sys import argv
+"""
+Lists all City objects from the database hbtn_0e_101_usa
+"""
+import sys
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    """
-    Script that lists all State objects and their corresponding City objects
-    from the database hbtn_0e_101_usa.
-    """
-    username, password, db_name = argv[1], argv[2], argv[3]
 
-    # Create MySQL engine using SQLAlchemy
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'
-        .format(username, password, db_name),
-        pool_pre_ping=True
-    )
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query to retrieve State and City information
-    result = (
-        session.query(State)
-        .order_by(State.id)
-        .all()
-    )
+    st = session.query(State).join(City).order_by(City.id).all()
 
-    # Display results
-    for state in result:
-        print("{}: {}".format(state.id, state.name))
+    for state in st:
         for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
-
-    # Close the session
-    session.close()
+            print("{}: {} -> {}".format(city.id, city.name, state.name))
