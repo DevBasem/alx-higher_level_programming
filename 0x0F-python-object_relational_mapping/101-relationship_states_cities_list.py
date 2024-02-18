affbcd205e726aaf56609ffe3935c6a9900e44df
DevBasem <engr.basemahmed@gmail.com>
2024-02-18 15:38:07 -0800

    task 2 adv

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
-"""Lists all State objects, and corresponding City objects"""
-
+"""List all states with corresponding cities"""
 from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    """
+    Script that lists all State objects and their corresponding City objects
+    from the database hbtn_0e_101_usa.
+    """
     username, password, db_name = argv[1], argv[2], argv[3]
 
-    # Database connection
+    # Create MySQL engine using SQLAlchemy
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'
         .format(username, password, db_name),
         pool_pre_ping=True
     )
-    Base.metadata.create_all(engine)
+
+    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
 
@@ -31,6 +35,7 @@ if __name__ == "__main__":
     for state in result:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+            print("\t{}: {}".format(city.id, city.name))
 
+    # Close the session
     session.close()
