4794457579ccd994185e73b7b99a82a64e331d04
DevBasem <engr.basemahmed@gmail.com>
2024-02-18 14:18:33 -0800

    pycodestyle fixes

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Script that adds the State object "Louisiana" to the database hbtn_0e_6_usa"""
+"""Script that adds the State object "Louisiana"
+to the database hbtn_0e_6_usa"""
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
