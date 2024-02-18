f8f7a11ec8ce24ba84a476484d2619439759ab4f
DevBasem <engr.basemahmed@gmail.com>
2024-02-18 09:02:55 -0800

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,49 +1,28 @@
 #!/usr/bin/python3
-"""
-Script that takes in the name of a state as an argument
-and lists all cities of that state, using the database hbtn_0e_4_usa.
-"""
+"""Lists cities by state"""
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    # Get command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(
+    conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=db_name,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
         charset="utf8"
     )
-
-    # Create a cursor object
-    cursor = db.cursor()
-
-    # Execute the query to select distinct cities of the specified state
+    cur = conn.cursor()
+    state_name = argv[4]
     query = """
-    SELECT GROUP_CONCAT(DISTINCT cities.name SEPARATOR ', ')
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC
+        SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
     """
-    cursor.execute(query, (state_name,))
-
-    # Fetch the result and display it
-    result = cursor.fetchone()
-    if result[0]:
-        print(result[0])
-    else:
-        print()
-
-    # Close cursor and database connection
-    cursor.close()
-    db.close()
+    cur.execute(query, (state_name, ))
+    cities = [row[0] for row in cur.fetchall()]
+    print(", ".join(cities))
+    cur.close()
+    conn.close()
