16ecefe44b8700bdb926fa63390f1a305a197c3d
DevBasem <engr.basemahmed@gmail.com>
2024-02-29 15:09:52 -0800

    task 2 adv

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL and displays the body of the response
-
-# Check if the file is a valid JSON
-if python3 -m json.tool "$2" > /dev/null 2>&1; then
-    curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
-else
-    echo -n "Not a valid JSON"
-fi
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
