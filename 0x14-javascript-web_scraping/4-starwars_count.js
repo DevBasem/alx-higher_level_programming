4c9a290445ec0ce2d417b90f153ac6773932205d
DevBasem <engr.basemahmed@gmail.com>
2024-03-26 07:18:46 -0700

    task 4 fx5

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
 
-const apiUrl = process.argv[2];
-
-request(apiUrl, function (error, response, body) {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Invalid response:', response.statusCode);
-  } else {
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
     const films = JSON.parse(body).results;
-    const wedgeFilms = films.filter(film =>
-      film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
-    );
-    console.log(wedgeFilms.length);
+    let count = 0;
+    for (const filmIndex in films) {
+      const filmChars = films[filmIndex].characters;
+      for (const charIndex in filmChars) {
+        if (filmChars[charIndex].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
