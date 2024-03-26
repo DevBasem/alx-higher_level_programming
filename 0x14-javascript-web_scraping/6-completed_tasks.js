c1a2a34fe788936aec30227435ffcdb8e1ba4a7f
DevBasem <engr.basemahmed@gmail.com>
2024-03-26 07:43:28 -0700

    task 5

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Invalid response:', response.statusCode);
+  } else {
+    const tasks = JSON.parse(body);
+    const completedTasksByUser = {};
+
+    tasks.forEach(task => {
+      if (task.completed) {
+        if (completedTasksByUser[task.userId]) {
+          completedTasksByUser[task.userId]++;
+        } else {
+          completedTasksByUser[task.userId] = 1;
+        }
+      }
+    });
+
+    console.log(completedTasksByUser);
+  }
+});
