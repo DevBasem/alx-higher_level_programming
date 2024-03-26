f97d51ae9dc22bc2273aab7af041d8293c3a8899
DevBasem <engr.basemahmed@gmail.com>
2024-03-26 07:14:06 -0700

    task 4 fx3

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,41 +4,19 @@ const request = require('request');
 
 const apiUrl = process.argv[2];
 const characterId = '18';
+const characterUrl = `https://swapi-api.alx-tools.com/api/people/${characterId}/`;
 
 request(apiUrl, function (error, response, body) {
   if (error) {
-    console.error('Error occurred while making the request:', error);
+    console.error(error);
     return;
   }
-
-  if (response.statusCode !== 200) {
-    console.error('Unexpected status code:', response.statusCode);
-    return;
-  }
-
-  let filmsData;
-  try {
-    filmsData = JSON.parse(body).results;
-  } catch (parseError) {
-    console.error('Error parsing API response:', parseError);
-    return;
-  }
-
-  if (!Array.isArray(filmsData)) {
-    console.error('Unexpected API response format: filmsData is not an array');
-    return;
-  }
-
-  let count = 0;
-  for (const film of filmsData) {
-    if (!Array.isArray(film.characters)) {
-      console.error('Unexpected film format: characters list not found or not an array');
-      continue;
+  const filmsData = JSON.parse(body).results;
+  const count = filmsData.reduce((acc, film) => {
+    if (film.characters.find(character => character === characterUrl)) {
+      return acc + 1;
     }
-    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-      count++;
-    }
-  }
-
+    return acc;
+  }, 0);
   console.log(count);
 });
