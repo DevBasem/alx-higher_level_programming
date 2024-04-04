91e1a8181b927b3bad3cfa985d11ec5af125abb6
DevBasem <engr.basemahmed@gmail.com>
2024-04-03 18:10:10 -0700

    task 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $.get("https://swapi-api.alx-tools.com/api/films/?format=json", function (data) {
+        data.results.forEach((film) => {
+            $('#list_movies').append(`<li>${film.title}</li>`);
+        });
+    });
+});
